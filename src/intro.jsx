66840d56c2fc7aff4c8b6b63794c373b7f8ef983
Vrushant <vrushantmukherjee@gmail.com>
2024-08-18 20:08:32 +0530

    a

--- src/intro.jsx
+++ src/intro.jsx
@@ -11,9 +11,9 @@ function Intro() {
       <h1 className='vrushant'><TypingEffect text="Hi, I am Vrushant Mukherjee!" speed={100} /></h1>
       <hr></hr>
       <h2 className='subtext'>A Passionate Computer Science Student</h2>
-      <a   className='mylinks' href = 'https://github.com/Vrushant14' target='_blank'><img className='githublogo' src='/assets/github.svg'></img></a>
-      <a  className='mylinks' href = 'https://linkedin.com/in/vrushantmukherjee14' target='_blank'><img className='githublogo' src='/assets/linkedin.svg'></img></a>
-      <a  className='mylinks' href= 'https://leetcode.com/u/Vrushant14/' target='_blank'><img className='githublogo' src='/assets/leetcode.svg'></img></a>
+      <a   className='mylinks' href = 'https://github.com/Vrushant14' target='_blank'><img className='githublogo' src='github.svg'></img></a>
+      <a  className='mylinks' href = 'https://linkedin.com/in/vrushantmukherjee14' target='_blank'><img className='githublogo' src='linkedin.svg'></img></a>
+      <a  className='mylinks' href= 'https://leetcode.com/u/Vrushant14/' target='_blank'><img className='githublogo' src='leetcode.svg'></img></a>
       
     </>
   )
